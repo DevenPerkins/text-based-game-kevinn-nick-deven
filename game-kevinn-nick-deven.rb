253a0de13c5c027c6abfad59b10a7d85ae3c5ca6
nickdsaunders <nickdavidsaunders@gmail.com>
2021-03-03 11:17:59 -0800

    added framework of story

---
+++ game-kevinn-nick-deven.rb
@@ -0,0 +1,166 @@
+# options = [1, 2, 3, 4, 5]
+# puts options.sample
+
+
+# Custom method that will print out each character one by one and then save the user's input in a variable:
+
+# def start_game
+#     'Here is the set up for our story. Please enter your name:'.each_char do |value|
+#       putc value
+#       sleep 0.05
+#     end
+#     puts
+#     gets.chomp
+#   end
+#   player_name = start_game
+
+
+# Day in the life
+# Wake up to alarm 7am options [snooze or get up]
+
+#            _________________________
+#          ,'        _____            `.
+#        ,'       _.'_____`._           `.
+#       :       .'.-'  12 `-.`.           \
+#       |      /,' 11  .   1 `.\           :
+#       ;     // 10    |     2 \\          |
+#     ,'     ::        |        ::         |
+#   ,'       || 9   ---O      3 ||         |
+#  /         ::                 ;;         |
+# :           \\ 8           4 //          |
+# |            \`. 7       5 ,'/           |
+# |             '.`-.__6__.-'.'            |
+# :              ((-._____.-))             ;
+# \             _))       ((_            /
+#  `.          '--'       '--'         ,'
+#    `.______________________________,'
+# ,-.
+# `-'
+#   O
+#    o
+#     .     ____________
+#    ,('`)./____________`-.-,|
+#   |'-----\\--------------| |
+#   |_______^______________|,|
+#   |                      |   SSt
+
+# Snooze once 20min wake up at 7:20, you missed the morning news.
+
+# Get up head to the living room and turn on the news see apocolypse.
+# o
+# o    |
+#  \   |
+#   \  |
+#    \.|-.
+#    (\|  )
+# .==================.
+# | .--------------. |
+# | |::.::.::.::.::| |
+# | |'::'::'::'::':| |
+# | |::.::.::.::.::| |
+# | |:'::'::'::'::'| |
+# | |::.::.::.::.::| |
+# | '--------------'o|
+# | LI LI """""""   o|
+# |==================|
+# jgs |  .------------.  |
+# | /              \ |
+# |/                \|
+# "                  "
+
+# Take dog for a walk noticing neighbors packign up belongings - [talk to neighbors, yes/no] 
+# if yes p decide to be social gain intel on zombie apocolypse
+# if no p tune the world out 
+
+# Go to the gym [strength, cardio] -  !positive outcome?
+# strength - gain points
+# cardio - tired loose points
+
+#             _( }
+#     -=  _  <<  \
+#       `.\__/`/\\
+#  -=     '--'\\  `
+#       -=    //
+#             \)
+
+    #                   *umph*
+
+    #                     |
+    #     _            \    /             _
+    #   _|#|       .-.         .-.       |#|_
+    #  |#|#|______/  /_ .-'-. _\  \______|#|#|
+    # [|#|#|------| (  || | ||  ) |------|#|#|]
+    #  |#|#|      |__|_.-'''-._|__|    Pr|#|#|
+    #   "|#|                           59|#|"
+    #     "                               "
+
+# Go to store notice all canned foods are gone hmm strange [xbox controller, toilet paper, weapons] - if miss news dont show weapons - ternary
+
+#  .--'""""""""""`--. 
+# ,'     .------.     `,
+# :     (        )     :
+# |\     `------'     /|
+# | `--.__________,--' |
+# |          /         |
+# |          \         |
+# |          /         |
+# |          \         |
+# `.         /        .'
+#   `--._____\____,--'
+
+#       _=====_                               _=====_
+#      / _____ \                             / _____ \
+#    +.-'_____'-.---------------------------.-'_____'-.+
+#   /   |     |  '.        S O N Y        .'  |  _  |   \
+#  / ___| /|\ |___ \                     / ___| /_\ |___ \
+# / |      |      | ;  __           _   ; | _         _ | ;
+# | | <---   ---> | | |__|         |_:> | ||_|       (_)| |
+# | |___   |   ___| ;SELECT       START ; |___       ___| ;
+# |\    | \|/ |    /  _     ___      _   \    | (X) |    /|
+# | \   |_____|  .','" "', |___|  ,'" "', '.  |_____|  .' |
+# |  '-.______.-' /       \ANALOG/       \  '-._____.-'   |
+# |               |       |------|       |                |
+# |              /\       /      \       /\               |
+# |             /  '.___.'        '.___.'  \              |
+# |            /                            \             |
+#  \          /                              \           /
+#   \________/                                \_________/
+
+
+# Get home from grocery store 
+
+# Make dinner
+
+       _
+      / )
+|||| / /
+||||/ /
+\__(_/
+ ||//
+ ||/
+ ||
+(||  
+ ""
+
+# After dinner activities [COD, Beer, Sleep, Prep for zombies]
+
+
+            #                    .....            
+            #                    C C  /            
+            #                   /<   /             
+            #    ___ __________/_#__=o             
+            #   /(- /(\_\________   \              
+            #   \ ) \ )_      \o     \             
+            #   /|\ /|\       |'     |             
+            #                 |     _|             
+            #                 /o   __\             
+            #                / '     |             
+            #               / /      |             
+            #              /_/\______|             
+            #             (   _(    <              
+            #              \    \    \             
+            #               \    \    |            
+            #                \____\____\           
+            #                ____\_\__\_\          
+            #              /`   /`     o\          
+            #              |___ |_______|..
